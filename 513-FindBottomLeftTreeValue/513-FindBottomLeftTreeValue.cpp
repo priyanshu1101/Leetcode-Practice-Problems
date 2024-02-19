f1ded28f4740dbd4948d348c503fbf9c931b597e
priyanshu1101 <77241116+priyanshu1101@users.noreply.github.com>
2024-02-20 01:35:12 +0530

    Update file 2/20/2024, 1:35:12 AM

--- 513-FindBottomLeftTreeValue/513-FindBottomLeftTreeValue.cpp
+++ 513-FindBottomLeftTreeValue/513-FindBottomLeftTreeValue.cpp
@@ -1,14 +1,15 @@
-            ans=root->val;
-            maxHeight=height;
-        };
-        ansFetcher(root->left,height+1,maxHeight,ans);
-        ansFetcher(root->right,height+1,maxHeight,ans);
-    }
-    int findBottomLeftValue(TreeNode* root) {
-        int ans=0;
-        int maxHeight=INT_MIN;
-        ansFetcher(root,0,maxHeight,ans);
-        return ans;
-    }
-};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void ansFetcher(TreeNode* root,int height,int &maxHeight,int &ans)
 [2,1,3]
