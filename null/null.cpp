64b1276bb3ea85cd8f15779c016d2fb11a6b236d
priyanshu1101 <77241116+priyanshu1101@users.noreply.github.com>
2024-02-20 01:34:02 +0530

    Create file

---
+++ null/null.cpp
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void ansFetcher(TreeNode* root,int height,int &maxHeight,int &ans)
+[2,1,3]
