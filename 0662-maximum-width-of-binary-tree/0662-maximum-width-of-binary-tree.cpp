3fb99c1b8d1a15f0dd2d4e6dcfbc9338ad26b35b
priyanshu1101 <77241116+priyanshu1101@users.noreply.github.com>
2024-02-29 18:09:46 +0530

    Time: 6 ms (69.14%), Space: 18.2 MB (34.94%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -6,37 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if (!root)
-            return 0;
-        queue<pair<TreeNode*, long long>> q;
-        q.push({root, 0});
-        int maxWidth = 0;
-        while (!q.empty()) {
-            int size = q.size();
-            int start, end;
-            int mmin = q.front().second;
-            for (int j = 0; j < size; j++) {
-                long long i = q.front().second - mmin;
-                TreeNode* temp = q.front().first;
+        queue<pair<TreeNode*,int>>q;
+        q.push({root,1});
+        int ans=0;
+        while(!q.empty()){
+            int start,end;
+            int size=q.size();
+            int mmin=q.front().second;
+            for(int i=0;i<size;i++){
+                TreeNode* temp=q.front().first;
+                int curr=q.front().second;
                 q.pop();
-                if (j == 0)
-                    start = i;
-                if (j == size - 1)
-                    end = i;
-                if (temp->left)
-                    q.push({temp->left, i * 2 + 1});
-                if (temp->right)
-                    q.push({temp->right, (i * 2) + 2});
+                if(i==0) start=curr;
+                if(i==size-1) end=curr;
+                if(temp->left) q.push({temp->left,(long long)(curr-mmin)*2+1});
+                if(temp->right) q.push({temp->right,(long long)(curr-mmin)*2+2});
             }
-            maxWidth = max(maxWidth, end - start + 1);
+            ans=max(ans,end-start+1);
         }
-        return maxWidth;
+        return ans;
     }
 };
