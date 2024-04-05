43d6a4f3c7ba18daa55840677be948c4b410f0f1
priyanshu1101 <77241116+priyanshu1101@users.noreply.github.com>
2024-04-05 14:14:01 +0530

    Time: 8 ms (8.39%), Space: 9.7 MB (5.04%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -7,7 +7,7 @@ public:
             int length=s.length();
             for(int i=0;i<length-1;i++)
             {
-                if( (islower(s[i])||islower(s[i+1])) && (isupper(s[i])||isupper(s[i+1])) && tolower(s[i])==tolower(s[i+1]))
+                if((islower(s[i])||islower(s[i+1])) && (isupper(s[i])||isupper(s[i+1])) && tolower(s[i])==tolower(s[i+1]))
                 {
                     s=s.substr(0,i)+s.substr(i+2,s.length()-i-2);
                     break;
