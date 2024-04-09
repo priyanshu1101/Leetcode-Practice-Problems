7408eaa4a851eb7d6ec16f64533893b1698e623d
priyanshu1101 <77241116+priyanshu1101@users.noreply.github.com>
2024-04-09 22:47:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int ans=0;
+        while(tickets[k]!=0){
+            for(int i=0;i<tickets.size();i++){
+                if(tickets[i]!=0){
+                    ans++;
+                    tickets[i]--;
+                }
+            }
+        }
+        return ans;
+    }
+};
