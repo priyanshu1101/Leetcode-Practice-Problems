f51d4b57f4a1bd23061d5df13a90a7edda4f56d2
priyanshu1101 <77241116+priyanshu1101@users.noreply.github.com>
2024-04-16 10:50:01 +0530

    Time: 4 ms (63.29%), Space: 9.2 MB (81.7%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,60 +1,42 @@
 class Solution {
 public:
-    bool canIPlaceHere(int i,int j,vector<vector<int>> board)
-    {
-        for(int k=i;k>=0;k--)
-        {
-            if(board[k][j]==1)
-                return false;
-        }
+    bool canIPlaceHere(vector<string> &board,int i,int j){
+        for(int k=0;k<board.size();k++) if(board[i][k]=='Q' || board[k][j]=='Q') return false;
         // Left Diagonal
         int k=i,l=j;
         while(k!=-1 && l!=-1)
         {
-            if(board[k--][l--]==1)
+            if(board[k--][l--]=='Q')
                 return false;
         }
         // Right Diagonal
         k=i,l=j;
         while(k!=-1 && l!=board.size())
         {
-            if(board[k--][l++]==1)
+            if(board[k--][l++]=='Q')
                 return false;
         }
         return true;
     }
-    void nqueenSolution(int i,vector<vector<int>> board,vector<vector<string>> &ans)
-    {
-        if(i==board.size())
-        {
-            vector<string> subans;
-            for(int k=0;k<board.size();k++)
-            {
-                string row="";
-                for(int l=0;l<board.size();l++)
-                {
-                    char ch = (board[k][l]==1)? 'Q':'.';
-                    row+=ch;
-                }
-                subans.push_back(row);
-            }
-            ans.push_back(subans);
-            return;
-        }
-        for(int j=0;j<board.size();j++)
-        {
-            if(canIPlaceHere(i,j,board))
-            {
-                board[i][j]=1;
-                nqueenSolution(i+1,board,ans);
-                board[i][j]=0;
+    bool nQueen(vector<vector<string>> &ans,vector<string> &board,int i){
+        if(i==board.size()){
+            ans.push_back(board);
+            return false;
+        };
+        for(int j=0;j<board.size();j++){
+            if(canIPlaceHere(board,i,j)){
+                board[i][j]='Q';
+                bool canWePlaceAhead=nQueen(ans,board,i+1);
+                if(canWePlaceAhead) return true;
+                board[i][j]='.';
             }
         }
+        return false;
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<vector<int>> board(n,vector<int>(n,0));
-        nqueenSolution(0,board,ans);
+        vector<string> board(n,string(n,'.'));
+        nQueen(ans,board,0);
         return ans;
     }
 };
