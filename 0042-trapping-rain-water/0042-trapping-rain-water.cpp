a5b5a4e963105a7c36865ab7eb640433ac5e1143
priyanshu1101 <77241116+priyanshu1101@users.noreply.github.com>
2024-04-16 09:53:48 +0530

    Time: 15 ms (20.11%), Space: 22.4 MB (62.88%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int l=0,r=height.size()-1,leftMax=0,rightMax=0;
-
-        int ans=0;
+        int l=0,r=height.size()-1,leftMax=0,rightMax=0,ans=0;
         while(l<r){
             if(height[l]<height[r]){
                 if(height[l]<leftMax) ans+=leftMax-height[l];
