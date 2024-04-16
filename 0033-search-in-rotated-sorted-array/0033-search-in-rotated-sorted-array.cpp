4568aceb72ea99be32ef73ba031c5dc70d6ed576
priyanshu1101 <77241116+priyanshu1101@users.noreply.github.com>
2024-04-16 10:17:47 +0530

    Time: 7 ms (16.75%), Space: 13.4 MB (96.76%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -5,18 +5,23 @@ public:
         while(start<=end){
             int mid=(start+end)/2;
             if(nums[mid]==target) return mid;
-            else if(nums[start]<=nums[mid]){ //Left Sorted
-                if(nums[start]<=target && nums[mid]>=target){
-                    end=mid-1;
+            else{
+                if(nums[start]<=nums[mid]){ // left sorted
+                    if(nums[start]<=target && nums[mid]>=target){
+                        end=mid-1;
+                    }
+                    else{
+                        start=mid+1;
+                    }
                 }
-                else start=mid+1;
-            }
-            else{   //Right Sorted
-                if(nums[mid]<=target && nums[end]>=target)
-                {
-                    start=mid+1;
+                else{ // right sorted
+                    if(nums[mid]<=target && target<=nums[end]){
+                        start=mid+1;
+                    }
+                    else{
+                        end=mid-1;
+                    }
                 }
-                else end=mid-1;
             }
         }
         return -1;
