235eca1ed5cae1b729c1d08b82c65dd7e38ee712
priyanshu1101 <77241116+priyanshu1101@users.noreply.github.com>
2024-04-16 06:19:54 +0530

    Time: 2193 ms (5.01%), Space: 502.5 MB (5%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,71 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end()); // T=O(NLogN)
         vector<vector<int>> ans;
-        if(nums.size()<3)
-            return ans;
-        sort(nums.begin(),nums.end());
-        int start=0;
-        int prev;
-        int ptr1=start+1;
-        int ptr2=nums.size()-1;
-        while(start<=nums.size()-3)
-        {
-            if(ptr1==ptr2)
-            {
-                prev=nums[start];
-                start++;
-                while(start<=nums.size()-3 && nums[start]==prev)
-                {
-                    prev=nums[start];
+        set<vector<int>> st;
+        for(int i=0;i<nums.size()-2;i++){
+            int start=i+1,end=nums.size()-1;
+            while(start<end){
+                int sum=nums[i]+nums[start]+nums[end];
+                if(sum==0){
+                    st.insert({nums[i],nums[start],nums[end]});
                     start++;
                 }
-                ptr1=start+1;
-                ptr2=nums.size()-1;
-            }
-            else
-            {
-                if(nums[start]+nums[ptr1]+nums[ptr2]==0)
-                {
-                    vector<int> subans;
-                    subans.push_back(nums[start]);
-                    subans.push_back(nums[ptr1]);
-                    subans.push_back(nums[ptr2]);
-                    ans.push_back(subans);
-                    
-                    
-                    prev=nums[start];
-                    start++;
-                    while(start<=nums.size()-3 && nums[start]==prev)
-                    {
-                        prev=nums[start];
-                        start++;
-                    }
-                }
-                else if(nums[start]+nums[ptr1]+nums[ptr2]>0)
-                {
-                    ptr2--;
-                }
-                else if(nums[start]+nums[ptr1]+nums[ptr2]<0)
-                {
-                    ptr1++;
-                }
+                else if(sum<0) start++;
+                else end--;
             }
         }
-        
-        
-        
-        
-        // vector<int> subans;
-        // subans.push_back(-1);
-        // subans.push_back(-1);
-        // subans.push_back(2);
-        // ans.push_back(subans);
-        // subans.clear();
-        // subans.push_back(0);
-        // subans.push_back(-1);
-        // subans.push_back(1);
-        // ans.push_back(subans);
+        for(auto itr=st.begin();itr!=st.end();itr++){
+            ans.push_back(*itr);
+        }
         return ans;
     }
 };
