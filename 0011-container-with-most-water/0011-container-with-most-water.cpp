024af0e82bf4abbc2da6e564a63033f976e1d085
priyanshu1101 <77241116+priyanshu1101@users.noreply.github.com>
2024-05-09 16:33:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -5,10 +5,10 @@ public:
         vector<int> ltr(height);
         vector<int> rtl(height);
         for(int i=1;i<height.size();i++){
-            ltr[i]=max(height[i],ltr[i-1]);
+            ltr[i]=max(ltr[i],ltr[i-1]);
         }
         for(int i=height.size()-2;i>=0;i--){
-            rtl[i]=max(rtl[i+1],height[i]);
+            rtl[i]=max(rtl[i+1],ltr[i]);
         }
         int left=0,right=height.size()-1;
         while(left<right){
