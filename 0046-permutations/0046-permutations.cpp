d404cbaae743fc1462714911881e01c3b53390e9
priyanshu1101 <77241116+priyanshu1101@users.noreply.github.com>
2025-02-03 16:55:11 +0530

    Time: 7 ms (7.57%), Space: 12.9 MB (7.51%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void ansFinder(vector<int>nums,int i){
-        if(i==nums.size()-1){
-            ans.push_back(nums);
+    bool isEmpty(vector<int> nums){
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=INT_MIN) return false;
+        }
+        return true;
+    }
+    void ansFinder(vector<int> nums,vector<vector<int>>& ans,vector<int> subans){
+        if(isEmpty(nums)){
+            ans.push_back(subans);
             return;
         }
-        for(int j=i;j<nums.size();j++){
-            swap(nums[i],nums[j]);
-            ansFinder(nums,i+1);
-            swap(nums[i],nums[j]);
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==INT_MIN) continue;
+            int value = nums[i];
+            subans.push_back(value);
+            nums[i]=INT_MIN;
+            ansFinder(nums,ans,subans);
+            nums[i]=value;
+            subans.pop_back();
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-       ansFinder(nums,0);
-       return ans; 
+        vector<vector<int>> ans;
+        vector<int> subans;
+        ansFinder(nums,ans,subans);
+        return ans;
     }
 };
