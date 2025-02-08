6e736466ad1736b5950848c9964bf357ae3eba91
priyanshu1101 <77241116+priyanshu1101@users.noreply.github.com>
2025-02-08 21:06:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -3,19 +3,18 @@ public:
     double myPow(double x, int n) {
         double ans = 1;
         bool neg=n<0;
-        long long int nn=abs(n);
-        cout<<nn;
-        while(nn)
+        n=abs(n);
+        while(n)
         {
-            if(nn%2==0)
+            if(n%2==0)
             {
                 x*=x;
-                nn=nn/2;
+                n=n/2;
             }
             else
             {
                 ans*=x;
-                nn--;
+                n--;
             }
         }
         return neg? 1/ans:ans;
